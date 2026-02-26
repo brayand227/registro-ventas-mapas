0384a1adf7fab63265fbdbace450a892e1dbc6fe
brayand227 <brayancalle227@gmail.com>
2026-02-25 20:40:28 -0500

    Agregando vista ventas como pagina principal

---
+++ src/main/java/com/ventas/registroventas/controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.ventas.registroventas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String mostrarVentas() {
+        return "ventas"; // SIN .html
+    }
+}
